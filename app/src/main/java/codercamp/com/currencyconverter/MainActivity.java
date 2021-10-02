3a486e22b602d337b2cb96c09d314d6cdf0b1a0b
Md. Shamim Hossain <59786807+shamimolmg123@users.noreply.github.com>
2021-10-02 22:54:29 +0600

    Remove All Warning

--- app/src/main/java/codercamp/com/currencyconverter/MainActivity.java
+++ app/src/main/java/codercamp/com/currencyconverter/MainActivity.java
@@ -1,5 +1,6 @@
 package codercamp.com.currencyconverter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -11,6 +12,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.JsonObject;
@@ -36,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Initialization
-        currencyToBeConverted = (EditText) findViewById(R.id.currency_to_be_converted);
-        convertToDropdown = (Spinner) findViewById(R.id.convert_to);
-        convertFromDropdown = (Spinner) findViewById(R.id.convert_from);
+        currencyToBeConverted = findViewById(R.id.currency_to_be_converted);
+        convertToDropdown = findViewById(R.id.convert_to);
+        convertFromDropdown = findViewById(R.id.convert_from);
         currencyConvertedResult = findViewById(R.id.currency_convertedResult);
         button = findViewById(R.id.button);
 
@@ -46,58 +48,53 @@ public class MainActivity extends AppCompatActivity {
         //Adding Functionality
         //String[] dropDownList = {"USD", "INR", "EUR", "NZD", "BDT"};
         String[] dropDownList = getResources().getStringArray(R.array.array_currency_codes);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, dropDownList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dropDownList);
         convertToDropdown.setAdapter(adapter);
         convertFromDropdown.setAdapter(adapter);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(v -> {
 
-            @Override
-            public void onClick(View v) {
+            if (currencyToBeConverted.getText().toString().isEmpty()) {
+                currencyToBeConverted.setError("Please Enter Value");
+                currencyToBeConverted.requestFocus();
+                button.stopAnimation();
+            } else {
+                button.startAnimation();
+                RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
+                Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
+                call.enqueue(new Callback<JsonObject>() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onResponse(@NonNull Call<JsonObject> call, @NonNull Response<JsonObject> response) {
+                        JsonObject res = response.body();
+                        assert res != null;
+                        JsonObject rates = res.getAsJsonObject("rates");
+                        double currency = Double.parseDouble(currencyToBeConverted.getText().toString());
+                        double multiplier = Double.parseDouble(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
+                        double result = currency * multiplier;
+                        currencyConvertedResult.setVisibility(View.VISIBLE);
+                        currencyConvertedResult.setText(currencyToBeConverted.getText().toString() + " " + convertFromDropdown.getSelectedItem().toString() + " = " + result + " " + convertToDropdown.getSelectedItem().toString());
+                        button.stopAnimation();
+                        button.revertAnimation();
+                    }
 
-                if (currencyToBeConverted.getText().toString().isEmpty()) {
-                    currencyToBeConverted.setError("Please Enter Value");
-                    currencyToBeConverted.requestFocus();
-                    button.stopAnimation();
-                } else {
-                    button.startAnimation();
-                    RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
-                    Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
-                    call.enqueue(new Callback<JsonObject>() {
-                        @Override
-                        public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                            JsonObject res = response.body();
-                            assert res != null;
-                            JsonObject rates = res.getAsJsonObject("rates");
-                            double currency = Double.valueOf(currencyToBeConverted.getText().toString());
-                            double multiplier = Double.valueOf(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
-                            double result = currency * multiplier;
-                            currencyConvertedResult.setVisibility(View.VISIBLE);
-                            currencyConvertedResult.setText(String.valueOf(currencyToBeConverted.getText().toString() + " " + convertFromDropdown.getSelectedItem().toString() + " = " + result + " " + convertToDropdown.getSelectedItem().toString()));
-                            button.stopAnimation();
-                            button.revertAnimation();
+                    @Override
+                    public void onFailure(@NonNull Call<JsonObject> call, @NonNull Throwable t) {
+                        button.stopAnimation();
+                        if (!isConnected()) {
+                            Toast.makeText(MainActivity.this, "Please Check Your Internet Connection", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error" + t.getMessage(), Toast.LENGTH_SHORT).show();
+
                         }
-
-                        @Override
-                        public void onFailure(Call<JsonObject> call, Throwable t) {
-                            button.stopAnimation();
-                            if (!isConnected()) {
-                                Toast.makeText(MainActivity.this, "Please Check Your Internet Connection", Toast.LENGTH_SHORT).show();
-                                button.stopAnimation();
-                                button.revertAnimation();
-                            } else {
-                                Toast.makeText(MainActivity.this, "Error" + t.getMessage(), Toast.LENGTH_SHORT).show();
-                                button.stopAnimation();
-                                button.revertAnimation();
-
-                            }
-                        }
-                    });
-                }
-
-
+                        button.stopAnimation();
+                        button.revertAnimation();
+                    }
+                });
             }
+
+
         });
 
 
@@ -113,6 +110,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             //Log.e("Connectivity Exception", e.getMessage());
         }
-        return connected;
+        return false;
     }
 }
